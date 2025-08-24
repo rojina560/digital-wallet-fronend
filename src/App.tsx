adb8c1e87493c71797a584ad267bf2f9e8ba8465
Rojina Akter <rojinaakter2795@gmail.com>
2025-08-24 09:36:44 +0600

    project-initialized

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+import { Button } from './components/ui/button'
+
+function App() {
+  
+  return (
+    <>
+    <Button> testing</Button>
+    </>
+  )
+}
+
+export default App
